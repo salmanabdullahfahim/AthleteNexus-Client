4187b93551312ff5628ecbc50a18b6fadb054de2
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-06-07 12:26:32 +0600

    authProvider added

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,5 +1,6 @@
 import React, { createContext } from 'react';
 import { getAuth } from "firebase/auth";
+import app from '../firebase/firebase.config';
 
 
 export const AuthContext = createContext(null);
@@ -7,6 +8,11 @@ export const AuthContext = createContext(null);
 const auth = getAuth(app);
 
 const AuthProvider = ({children}) => {
+
+
+    const authInfo = {
+
+    }
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
